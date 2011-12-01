9fad48bffcb70cc1fe600721b90d886562942eef
Scott Clark <sclark@camsys.com>
2011-12-01 17:53:18 -0500

    now runs the b63 reduce, properly zips GTFSs and replaces the routes.txt in the b63 gtfs

--- onebusaway-nyc-bundle-builder/src/main/script/buildBundle.bash
+++ onebusaway-nyc-bundle-builder/src/main/script/buildBundle.bash
@@ -2,22 +2,37 @@
 
 # Script created by Scott Clark to begin to script the bundle building process.
 BUNDLE_DATA_DIR=bundle_data
-ROUTESTXT_WITH_COLORS_FILENAME=si_routes_with_colors.txt
+B63_ROUTESTXT_WITH_COLORS_FILENAME=routes_b63_with_colors.txt
+SI_ROUTESTXT_WITH_COLORS_FILENAME=routes_si_with_colors.txt
 
 GTFS_DOWNLOAD_LOC=http://www.mta.info/developers/data/nyct/bus/
 GTFS_STATEN_ISLAND=google_transit_staten_island
 GTFS_BROOKLYN=google_transit_brooklyn
+GTFS_B63=gtfs-b63
 
 downloadFile () {
   file=$1
   if [ -z "$file" ] 
   then
     echo downloadFile requires one argument.
-    return -1
+    return 1
   fi
 
   echo Downloading File $file with wget:
   wget -nv $file
+}
+
+zipUpGtfs () {
+  gtfsName=$1
+
+  if [ ! -d $gtfsName ]
+  then
+    echo zipUpGtfs requires one argument, the directory of gtfs files to zip up.
+    return 1
+  fi
+
+  cd $gtfsName
+  zip -q ../${gtfsName}.zip *.txt && cd ..
 }
 
 echo "Starting Bundle Builder script."
@@ -52,6 +67,7 @@ fi
 
 cd $BUNDLE_DATA_DIR
 
+echo
 echo "Modify Staten Island GTFS"
 
 echo "Unzip staten island gtfs and replace routes.txt"
@@ -60,27 +76,42 @@ cd $GTFS_STATEN_ISLAND
 
 unzip -LL -q ../../$GTFS_STATEN_ISLAND.zip.original_from_mta
 
-mv routes.txt ../si_routes_from_original_gtfs.txt
-cp ../../$ROUTESTXT_WITH_COLORS_FILENAME routes.txt
+mv routes.txt ../routes_si_from_original_gtfs.txt
+cp ../../$SI_ROUTESTXT_WITH_COLORS_FILENAME routes.txt
 
 echo "zip up new Staten Island GTFS"
 
 cd ..
 
-zip -q $GTFS_STATEN_ISLAND.zip $GTFS_STATEN_ISLAND/*.txt
+zipUpGtfs $GTFS_STATEN_ISLAND
 rm $GTFS_STATEN_ISLAND/*
 rmdir $GTFS_STATEN_ISLAND
 
 echo "Done modifying Staten Island GTFS"
 
+echo
 echo "Turn Brooklyn GTFS -> B63 GTFS"
 echo "Running onebusaway-gtfs-transformer-cli "
-java -Xmx2G -jar ../lib/onebusaway-gtfs-transformer-cli-1.2.5.jar --transform=../transformer_modifications.txt ../$GTFS_BROOKLYN.zip.original_from_mta ./$GTFS_BROOKLYN
+java -Xmx2G -jar ../lib/onebusaway-gtfs-transformer-cli-1.2.5.jar --transform=../gtfs_transformer_modifications.txt ../$GTFS_BROOKLYN.zip.original_from_mta ./$GTFS_BROOKLYN
+
+echo 
+echo "Update the routes file in brooklyn->b63 GTFS"
+cd $GTFS_BROOKLYN 
+mv routes.txt ../routes_b63_from_original_reduced_gtfs.txt
+cp ../../$B63_ROUTESTXT_WITH_COLORS_FILENAME routes.txt
+cd ..
+
+echo "Zipping up B63 GTFS reduced from the Brooklyn GTFS"
+zipUpGtfs $GTFS_BROOKLYN
+rm $GTFS_BROOKLYN/*
+rmdir $GTFS_BROOKLYN
+
+mv $GTFS_BROOKLYN.zip $GTFS_B63.zip
+
+echo "Done modifying the B63 GTFS"
 
 echo 
 echo "Script finished, however I didnt do any of the following:"
-echo "->Reduce Brooklyn GTFS to just B63 GTFS"
-echo "->Insert color code into B63 GTFS routes.txt or replace routes.txt with b63_with_colors_routes.txt"
 echo "->Combine Staten Island and Brooklyn STIF files."
 echo "->Run python script on combined STIFs"
 echo "->Build bundle"
